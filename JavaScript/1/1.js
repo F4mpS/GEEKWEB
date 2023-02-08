77152bb4badc208ded5157881bb4a6278768aaa7
F4mpS <77170837+F4mpS@users.noreply.github.com>
2023-02-08 15:17:27 +0300

    Update 1.js

--- JavaScript/1/1.js
+++ JavaScript/1/1.js
@@ -1,4 +1,4 @@
 const celsius = Number.parseFloat(prompt("Введите грудусы цельсия"));
 const fahrenheit = (9 / 5) * celsius + 32;
-alert(`Цельсий: ${C} 
-Фаренгейт: ${F.toFixed(1)}`);
+alert(`Цельсий: ${celsius} 
+Фаренгейт: ${fahrenheit.toFixed(1)}`);
